0a4ed8d54ef8e5cf1b3df65951659e4a9b3bfdda
Manu <49009183@est.ort.edu.ar>
2026-05-26 15:38:20 -0300

    Update Ejercicio_1.ino

--- Ejercicio_1/Ejercicio_1.ino
+++ Ejercicio_1/Ejercicio_1.ino
@@ -4,6 +4,7 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <DHT.h>
+#include "time.h"
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
@@ -15,28 +16,51 @@ DHT dht(DHTPIN, DHTTYPE);
 
 const int SW1 = 35;
 const int SW2 = 34;
+float temperatura = 0;
+
+unsigned long startMillis;
+unsigned long currentMillis;
+unsigned long tiempoparasegundos = 1000;
+unsigned long segundos = 0;
+unsigned long minutos = 19;
+unsigned long horas = 15;
 
 enum Estados { MONITOR, CONFIG, DOSSW, SW1L, SW2L, DOSSW2 };
 Estados estadoActual = MONITOR;
 
-float temperatura = 0;
-int segundos = 1;
-int minuto = 30;
-int hora = 12;
-
 void setup() {
   pinMode(SW1, INPUT_PULLUP);
   pinMode(SW2, INPUT_PULLUP);
-  pinMode(LED, OUTPUT);
  
   dht.begin();
   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     for(;;);
   }
   display.clearDisplay();
+  Serial.begin(115200);
+  startMillis = millis();
+  
 }
 
 void loop() {
+  currentMillis = millis();
+  if (currentMillis - startMillis >= tiempoparasegundos){
+    segundos++;
+    startMillis = currentMillis;
+  }
+  if (segundos >= 60)
+  {
+    minutos++;
+    segundos = 0;
+  }
+  if (minutos >= 60)
+  {
+    horas++;
+    minutos = 0;
+  }
+  if (horas >= 24){
+    horas = 0;
+  }
   temperatura = dht.readTemperature();
   switch (estadoActual) {
     case MONITOR:
@@ -64,8 +88,8 @@ void loop() {
 
     case SW1L:
     if (digitalRead(SW1) == HIGH){
-      minuto++;
       estadoActual = CONFIG;
+      horas++;
     }
     if (digitalRead(SW2) == LOW){
       estadoActual = DOSSW2;
@@ -74,8 +98,8 @@ void loop() {
 
     case SW2L:
     if (digitalRead(SW2) == HIGH){
-      hora++;
       estadoActual = CONFIG;
+      minutos++;
     }
     if (digitalRead(SW1) == LOW){
       estadoActual = DOSSW2;
@@ -89,29 +113,28 @@ void loop() {
     break;
   }
 
+
 }
-void mostrarMonitor() {
+void mostrarMonitor() { // display modo pantalla 1
   display.clearDisplay();
   display.setTextSize(1);
+  display.setTextColor(WHITE);
   display.setCursor(0,0);
   display.println("MODO: MONITOR");
  
   display.setTextSize(1);
   display.setCursor(0,20);
-  display.print("Segundos: ");
-  display.print(segundos);
- 
-  display.setTextSize(1);
-  display.setCursor(0,30);
-  display.print("Minuto: ");
-  display.print(minuto);
-  display.display();
+  display.print("Hora: " );
+  display.print(horas);
+  display.print(":");
+  display.println(minutos);
 
   display.setTextSize(1);
-  display.setCursor(0,40);
-  display.print("Hora: ");
-  display.print(hora);
+  display.setCursor(0,50);
+  display.print("Temp: ");
+  display.print(temperatura, 1);
   display.display();
+  
 }
 
 
@@ -122,14 +145,10 @@ void ajustarUmbral() {
   display.println("MODO: CONFIG");
  
   display.setTextSize(1);
-  display.setCursor(0,20);
-  display.print("SET: ");
-  display.print(hora);
-  display.display();
-
-  display.setTextSize(1);
-  display.setCursor(0,40);
-  display.print("SET: ");
-  display.print(minuto);
+  display.setCursor(0,25);
+  display.print("" );
+  display.print(horas);
+  display.print(":");
+  display.print(minutos);
   display.display();
 }
